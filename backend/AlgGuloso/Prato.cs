e359f4a4f81577c3a3ca4e6b688fadfd9c3df70d
Ana Clara Bertoldo <101759772+bertoldoa3@users.noreply.github.com>
2024-06-04 08:58:14 -0300

    Create Prato.cs
    Update Prato

---
+++ backend/AlgGuloso/Prato.cs
@@ -0,0 +1,11 @@
+public class Prato
+{
+    public int Custo { get; set; }
+    public int Lucro { get; set; }
+
+    public Prato(int custo, int lucro)
+    {
+        Custo = custo;
+        Lucro = lucro;
+    }
+}
